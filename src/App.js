4479a5ac115f11360bebf44341d97adf307b4922
qkrwlgh123 <jiho10245@gmail.com>
2022-03-03 22:27:41 +0900

    페이지 별 라우팅, 제목 클릭에 따른 상세 정보 API 요청

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
 import { useEffect, useState } from 'react';
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './routes/Home';
+import Detail from './routes/Detail';
 function App() {
-  return null;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/movie/:id" element={<Detail />} />
+      </Routes>
+    </Router>
+  );
 }
 export default App;
