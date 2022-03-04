893a31377407ec44f6383cf2607b12a0cf4fab2f
qkrwlgh123 <jiho10245@gmail.com>
2022-03-04 10:50:16 +0900

    영화 제목 클릭 시 상세 정보

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,19 +1,52 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 function Detail() {
+  const [loading, setLoading] = useState(true);
+  const [movie, setMovie] = useState({});
   const { id } = useParams();
+
   const getMovie = async () => {
     const json = await (
       await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
     ).json();
-    console.log(json);
+    setMovie(json.data.movie);
+    setLoading(false);
   };
+
   useEffect(() => {
     getMovie();
   }, []);
-
-  return <h1>Detail</h1>;
+  console.log(movie);
+  return (
+    <div>
+      {loading ? (
+        <h1>loading...</h1>
+      ) : (
+        <div>
+          <img src={movie.medium_cover_image} />
+          <h2>{movie.title_long}</h2>
+          <h2>{movie.language}</h2>
+          <h3>{movie.rating} rating</h3>
+          <h3>{movie.runtime} minutes</h3>
+          <ul>
+            {movie.genres.map((item) => (
+              <li key={item}>{item}</li>
+            ))}
+          </ul>
+          <h3>{movie.description_intro}</h3>
+          <h1>Downloads</h1>
+          <ul>
+            {movie.torrents.map((item) => (
+              <a href={item.url}>
+                <li key={item.url}>{item.url}</li>
+              </a>
+            ))}
+          </ul>
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default Detail;
